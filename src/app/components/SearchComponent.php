8782c71e4d6db5c4725fd896471e1277659f39e2
= <=>
2022-04-05 16:26:54 +0530

    single page added & docs added

---
+++ src/app/components/SearchComponent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Components;
+
+/**
+ * helper class for hitting the api and getting response
+ */
+class SearchComponent
+{
+    /**
+     * returns whatever the response of the passed url
+     *
+     * @param [string] $q
+     * @return ARRAY
+     */
+    public function search($q)
+    {
+        $q = implode('+', explode(' ', $q));
+        $url = "https://openlibrary.org/search.json?q={$q}&mode=ebooks&has_fulltext=true";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $resp = curl_exec($ch);
+
+        $response = json_decode($resp, true);
+
+        return $response;
+    }
+    /**
+     * return an image path
+     *
+     * @param [string] $lending_edition_s
+     * @param [string] $size
+     * @return string
+     */
+    public function getImage($lending_edition_s, $size)
+    {
+        $image = "https://covers.openlibrary.org/b/olid/{$lending_edition_s}-{$size}.jpg";
+        return $image;
+    }
+    public function getBookById($q)
+    {
+        $url = "https://openlibrary.org/api/books?bibkeys=ISBN:{$q}&jscmd=details&format=json";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $resp = curl_exec($ch);
+
+        $response = json_decode($resp, true);
+
+        return $response;
+    }
+}
---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        $this->assets->addCss('css/styles.css');
+        //load the search component
+        $search = new App\Components\SearchComponent();
+        if ($this->request->isPost()) {
+            $data = $search->search($this->request->getPost()['search']);
+            // echo "<pre>";
+            // print_r($search->search($this->request->getPost()['search']));
+            // die();
+            // pass data into view
+            $this->view->data = $data;
+        }
+    }
+    public function bookAction()
+    {
+        $this->assets->addCss('css/styles.css');
+        $id = $this->request->getQuery()["id"];
+        if (!isset($id)) {
+            header("location:/");
+        }
+        //fetch the book content here
+        $search = new App\Components\SearchComponent();
+        $data = $search->getBookById($id);
+        $image = explode("/", $data['ISBN:' . $id]["preview_url"])[4];
+        $this->view->data = $data['ISBN:' . $id];
+        $this->view->image = "https://covers.openlibrary.org/b/olid/{$image}-L.jpg";
+        // echo "<pre>";
+        // print_r($data);
+        // echo "</pre>";
+        // die();
+    }
+}
